e7656f0eb95142835a735694be5f8db127781fe4
Michael Laryea <80675696+micky-4u@users.noreply.github.com>
2021-10-06 14:10:11 +0000

    Update SearchSortAlgo.java
    The selection Sort code

--- SearchSortAlgo.java
+++ SearchSortAlgo.java
@@ -1,5 +1,33 @@
-public class SearchSortAlgo{
-  public static main(String[] args){
-    
+
+public class SearchSortAlgo {
+
+  public static void selectionSort(int[] array) {
+    // Sorting in assending order
+    for (int i = 0; i < array.length - 1; i++) {
+      int minIndex = i;
+
+      for (int j = i + 1; j < array.length; j++) {
+        if (array[j] < array[minIndex]) {
+          minIndex = j;
+        }
+      }
+
+      // swaping elements
+      int minValue = array[minIndex];
+      array[minIndex] = array[i];
+      array[i] = minValue;
+    }
+    for (int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + "  ");
+    }
   }
+
+  public static void main(String[] args) {
+
+    int[] array = { 2, 1, 3, 5, 6, 8, 7, 45, 15, 35, 87 };
+
+    selectionSort(array);
+
+  }
+
 }
