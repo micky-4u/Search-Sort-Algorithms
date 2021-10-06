127274b2fb46c6960ef6409d2417b4712417104e
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2021-10-06 15:18:16 +0000

    Update SearchSortAlgo.java

--- SearchSortAlgo.java
+++ SearchSortAlgo.java
@@ -2,9 +2,12 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class SearchSortAlgo {
    public static void main(String args[]) {
+      //scanner used to take in user input
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter a string value: ");
       String str = sc.nextLine();
+
+      //converts the strings to integers for comparison
       char charArray[] = str.toCharArray();
       Arrays.sort(charArray);
       System.out.println(new String(charArray));
