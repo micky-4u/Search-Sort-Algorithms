9cf3a96399613c260bb2ab93f6c92a536997bc49
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2021-10-06 15:55:55 +0000

    Update SearchSortAlgo.java

--- SearchSortAlgo.java
+++ SearchSortAlgo.java
@@ -2,14 +2,26 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class SearchSortAlgo {
    public static void main(String args[]) {
-      //scanner used to take in user input
+      int temp, size;
+
+      //scanner is used to take user input
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter a string value: ");
       String str = sc.nextLine();
 
-      //converts the strings to integers for comparison
+      //strings converted to integers for comparison
       char charArray[] = str.toCharArray();
-      Arrays.sort(charArray);
-      System.out.println(new String(charArray));
+      size = charArray.length;
+      for(int i = 0; i < size; i++ ) {
+         for(int j = i+1; j < size; j++) {
+            if(charArray[i]>charArray[j]) {
+               temp = charArray[i];
+               charArray[i] = charArray[j];
+               charArray[j] = (char) temp;
+            }
+         }
+      }
+      // prints out the string sorted alphabetically
+      System.out.println("Alphabetical sorting is: "+Arrays.toString(charArray));
    }
 }
